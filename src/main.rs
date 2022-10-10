d6b145e7149a6c315ec0c60d0477e7684496ef7f
Johannes Kresner <johannes.kresner@check24.de>
2022-10-10 17:58:50 +0200

    fix: raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,19 @@
-use std::io::{stdin, Read};
+use crossterm::{terminal::{enable_raw_mode}, event::{read, Event, KeyCode, KeyModifiers}};
 
-use crossterm::terminal::{enable_raw_mode, is_raw_mode_enabled};
+
 
 fn main() {
-    if is_raw_mode_enabled().is_err() {
-        let raw = enable_raw_mode();
-        if raw.is_err() {
-            return;
-        }
-    }
+    let _raw = enable_raw_mode();
 
-    for b in stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("{:?}\r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
-                }
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
+    loop {
+        if let Ok(Event::Key(key)) = read() {
+            if key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('q') {
+                break;
+            } else if key.modifiers.intersects(KeyModifiers::all())  {
+                println!("{:?}\r", key.modifiers)
+            } else {
+                println!("{:?}\r", key.code)
             }
-            Err(err) => panic!("{}", err),
         }
     }
-}
-
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
 }
